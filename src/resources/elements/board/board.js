4062c70d91bb9f94d2bf8ab544568e6a037080c4
Esger <esgerj@gmail.com>
2023-08-29 21:18:53 +0200

    Game Pause met Escape

--- src/resources/elements/board/board.js
+++ src/resources/elements/board/board.js
@@ -4,8 +4,10 @@ import { KeyInputService } from 'services/key-input-service';
 
 @inject(EventAggregator, KeyInputService)
 export class BoardCustomElement {
+    _title = 'TypeFall';
 
     constructor(eventAggregator, keyInputService) {
+        this.title = this._title;
         this._eventAggregator = eventAggregator;
         this._keyInputService = keyInputService;
         this._letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -66,11 +68,13 @@ export class BoardCustomElement {
 
     _startGame() {
         this._letterAdderInterval = setInterval(_ => this._addRandomLetter(), this._addInterval);
+        this.title = this._title;
     }
 
     _pauseGame() {
         clearInterval(this._letterAdderInterval);
         this._letterAdderInterval = undefined;
+        this.title = 'Pause';
     }
 
     _togglePause() {
