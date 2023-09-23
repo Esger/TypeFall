89c28013c174aabe8835efed4dac6a78e530ed73
Esger <esgerj@gmail.com>
2023-09-23 14:40:17 +0200

    Talen en/nl/random

--- src/resources/elements/settings/settings.js
+++ src/resources/elements/settings/settings.js
@@ -1,23 +1,36 @@
-import { inject } from 'aurelia-framework';
+import { inject, observable } from 'aurelia-framework';
 import { EventAggregator } from 'aurelia-event-aggregator';
 
 @inject(EventAggregator)
 export class SettingsCustomElement {
+    @observable selectedLanguage;
 
     constructor(eventAggregator) {
         this._eventAggregator = eventAggregator;
         this.initial = true;
+        this.languages = [{
+            id: 'en',
+            name: 'English'
+        }, {
+            id: 'nl',
+            name: 'Dutch'
+        }, {
+            id: 'random',
+            name: 'Random mode'
+        }];
+        this.selectedLanguage = this.languages[0];
     }
 
     attached() {
         this._startGameSubscription = this._eventAggregator.subscribeOnce('startGame', _ => this.initial = false);
-    }
-
-    randomToggle() {
-        this._eventAggregator.publish('randomToggle', this.randomMode);
+        this._eventAggregator.publish('languageChanged', this.selectedLanguage.id);
     }
 
     startGame() {
         this._eventAggregator.publish('startGame');
+    }
+
+    selectedLanguageChanged(newValue) {
+        this._eventAggregator.publish('languageChanged', newValue.id);
     }
 }
