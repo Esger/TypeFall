5dc2d24f0738d9ae4dc416541f065e7a4cbe94a4
Esger <esgerj@gmail.com>
2023-10-06 01:57:25 +0200

    Fix bug knoppen triggeren pause ook

--- src/app.js
+++ src/app.js
@@ -35,12 +35,14 @@ export class App {
                 this._eventAggregator.publish('pause');
                 this._eventAggregator.publish('startGame');
             }, 50);
-        }).on('click', _ => this.initial ? this._eventAggregator.publish('startGame') : this._eventAggregator.publish('pause'));
+        })
+        $('.blocks').on('click', _ => this.initial ? this._eventAggregator.publish('startGame') : this._eventAggregator.publish('pause'));
     }
 
     detached() {
         this._startStopSubscription.dispose();
         this._startSubscription.dispose();
-        $(window).off('resize click');
+        $(window).off('resize');
+        $$('.blocks').off('click');
     }
 }
