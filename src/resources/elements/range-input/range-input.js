814ca46176f544dcd1eea6026931db34857da5a7
Esger <esgerj@gmail.com>
2023-10-15 00:04:08 +0200

    range-input element

---
+++ src/resources/elements/range-input/range-input.js
@@ -0,0 +1,18 @@
+import { bindable, inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
+export class RangeInputCustomElement {
+    @bindable value;
+    @bindable min = 1;
+    @bindable max = 3;
+    @bindable label;
+
+    constructor(eventAggregator) {
+        this._eventAggregator = eventAggregator;
+    }
+    valueChanged(newValue) {
+        this._eventAggregator.publish('level', newValue);
+    }
+}
+
